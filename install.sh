ccd4f42bf818862f699830c8a8695104e4660ccb
Aoxsin <110761903+aoxsin@users.noreply.github.com>
2025-04-25 18:06:43 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,6 @@ echo "Installing Dependencies..."
 sudo apt update
 sudo apt install git axel openjdk-21-jre openjdk-22-jre openjdk-23-jre -y
 
-# Cloning
-git clone https://github.com/xiv3r/Burpsuite-Professional.git 
-cd Burpsuite-Professional
 
 # Download Burpsuite Professional
 echo "Downloading Burpsuite Professional Latest..."
@@ -21,7 +18,7 @@ echo "Starting Key loader.jar..."
 
 # Execute Burpsuite Professional
 echo "Executing Burpsuite Professional..."
-echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > burpsuitepro
-chmod +x burpsuitepro
-cp burpsuitepro /bin/burpsuitepro
-(./burpsuitepro)
+echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > burp
+chmod +x burp
+cp burp /bin/burp
+(./burp)
